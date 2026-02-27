cd65bf7281b80e1006eaef9adaf1acf0150e713f
Arthur Santos <arthur.s.vale@gmail.com>
2026-02-27 09:19:15 -0300

    Criado a estrutura inicial do projeto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/Titulo"
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/arthursantos1.png" alt="Foto perfil Github" />
+    <Titulo>Arthur Santos</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,12 @@
+import Titulo from "../../components/Titulo";
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores neque pariatur magni temporibus quae eos ipsa, nam cum molestias enim consequatur. Rem hic perspiciatis harum tempora similique, natus accusantium est.
+    </p>
+  </section>
+)
+
+export default Sobre
