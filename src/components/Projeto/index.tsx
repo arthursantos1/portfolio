2a422ea977c0465ceb6b3c079637f28bfd83aa2e
Arthur Santos <arthur.s.vale@gmail.com>
2026-03-02 10:42:50 -0300

    criado a seção de projeto e estilizado

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from "../Titulo";
+import Paragrafo from "../Paragrafo";
+import { Card, LinkBotao } from "./styles";
+
+const Projeto = () => (
+  <Card>
+    <Titulo fontSize={16}>Projeto Lista de tarefas</Titulo>
+    <Paragrafo tipo="secundario">
+      Lista de tarefas feita com VueJS
+    </Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
