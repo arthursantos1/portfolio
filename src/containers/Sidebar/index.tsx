a3bae9ebfe278348c36aaf0f9df0d391299e8411
Arthur Santos <arthur.s.vale@gmail.com>
2026-03-03 09:32:26 -0300

    Criado diferente temas e também que o botão troque o tema

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,14 +3,18 @@ import Avatar from "../../components/Avatar";
 import Paragrafo from "../../components/Paragrafo";
 import { Descricao, BotaoTema, SidebarContainer } from "./styles";
 
-const Sidebar = () => (
+type Props = {
+  trocaTema: () => void;
+}
+
+const Sidebar = (props: Props) => (
   <aside>
     <SidebarContainer>
       <Avatar />
       <Titulo fontSize={20}>Arthur Santos</Titulo>
       <Paragrafo tipo="secundario" fontSize={16}>arthursantos1</Paragrafo>
       <Descricao tipo="principal" fontSize={12}>Engenheiro front-end</Descricao>
-      <BotaoTema>Trocar tema</BotaoTema>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
     </SidebarContainer>
   </aside>
 )
